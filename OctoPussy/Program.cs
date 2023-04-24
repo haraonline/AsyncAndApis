68fd6f4667624ab3918e48c35edc9cb3c53dd85e
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-24 19:15:33 +0200

    Add project files.

---
+++ OctoPussy/Program.cs
@@ -0,0 +1,27 @@
+﻿using Octokit;
+using System.Threading.Tasks;
+
+namespace OctoPussy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "haraonline";
+            IReadOnlyList<Repository> repos = ListRepos(userName).GetAwaiter().GetResult();
+            Console.WriteLine("Repositories for {0}:", userName);
+            Console.WriteLine("---------------------");
+            foreach (var repo in repos)
+            {
+                Console.WriteLine(repo.Name);
+            }
+        }
+
+        private static async Task<IReadOnlyList<Repository>> ListRepos(string userName)
+        {            
+            var client = new GitHubClient(new ProductHeaderValue("TheOctopusProject"));
+            var repos = await client.Repository.GetAllForUser(userName);
+            return repos;
+        }
+    }
+}
